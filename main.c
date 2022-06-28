560dfd21d80e3f1d48e0f76438cd751181c2c28d
Tatiana Pacheco <89342555+tatipacheco@users.noreply.github.com>
2022-06-28 18:23:59 -0300

    Atualizado 28/06

---
+++ main.c
@@ -0,0 +1,72 @@
+/*Alunas:
+Maria Eduarda Nothen Ruhe - 00287686
+Tatiana Pacheco de Almeida - 00252861 */
+
+#include <stdio.h>
+#include "tokens.h"
+
+int yylex(void);
+int yylex_destroy(void);
+void hashInit(void);
+void hashPrint(void);
+
+int main (int argc, char **argv)
+{
+  int token = 0;
+  if(argc < 2)
+{
+  fprintf(stderr, "Call ./a.out file_name\n");
+  exit(1);
+}
+
+yyin = fopen(argv[1],"r");
+
+hashInit();
+
+  while (token = yylex()) {
+    switch (token){
+		case ';':
+		case '(':
+		case ')':
+		case '[':
+		case ']':
+		case '{':
+		case '}':
+		case '+':
+		case '-':
+		case '.':
+		case '/':
+		case '<':
+		case '>':
+		case '&':
+		case '|':
+		case '~': printf("Caracter especial: %d\n", token); break;
+		case KW_CHAR: printf("KW_CHAR: %d\n",token); break;
+		case KW_INT: printf("KW_INT: %d\n",token); break;
+		case KW_FLOAT: printf("KW_FLOAT: %d\n",token); break;
+		case KW_IF: printf("KW_IF: %d\n",token); break;
+		case KW_ELSE: printf("KW_ELSE: %d\n",token); break;
+		case KW_WHILE: printf("KW_WHILE: %d\n",token); break;
+		case KW_READ: printf("KW_READ: %d\n",token); break;
+		case KW_PRINT: printf("KW_PRINT: %d\n",token); break;
+		case KW_RETURN: printf("KW_RETURN: %d\n",token); break;
+		case ASSIGNMENT: printf("ASSIGNMENT: %d\n",token); break;
+		case OPERATOR_LE: printf("OPERATOR_LE: %d\n",token); break;
+		case OPERATOR_GE: printf("OPERATOR_GE: %d\n",token); break;
+		case OPERATOR_EQ: printf("OPERATOR_EQ: %d\n",token); break;
+		case OPERATOR_DIF: printf("OPERATOR_DIF: %d\n",token); break;
+		case TK_IDENTIFIER: printf ("TK_IDENTIFIER: %d \n", token); break;
+		case TOKEN_ERROR: printf ("TOKEN_ERROR: %d \n", token); break;
+        	case LIT_INTEGER: printf ("LIT_INTEGER: %d \n", token); break;
+		case LIT_CHAR: printf ("LIT_CHAR: %d \n", token); break;
+		case LIT_FLOAT: printf ("LIT_FLOAT: %d \n", token); break;
+		case LIT_STRING: printf ("LIT_STRING: %d \n", token); break;
+		default: printf ("Invalid Token %d \n", token); return 1; break;
+	}
+  }
+	
+hashPrint();
+yylex_destroy();
+return 0;
+  
+}
